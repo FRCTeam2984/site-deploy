ddd5f63c40da50245a96aa671944ac0d5e0af4c6
Lukas Koziol <lukaskoziol126@gmail.com>
2019-02-04 16:11:36 -0800

    reversion

--- js/table.js
+++
@@ -1,7 +0,0 @@
-//adds 'table' class to all tables to apply bootstrap formatting
-document.addEventListener("DOMContentLoaded", function(event) {
-    var tables = document.querySelectorAll('table');
-    for (var i = 0; i < tables.length; i++) {
-        tables[i].className = "table";
-    }
-});
